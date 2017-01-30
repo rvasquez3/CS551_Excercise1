06abd987270596221640fbadd55a022990832a7c
rvasquez3 <r.vasquez44@yahoo.com>
2017-01-30 12:54:27 -0700

    Template for Exercise1

---
+++ Exercise1.py
@@ -0,0 +1,19 @@
+# git-exercise-01.py
+
+# TEAM LEADER:
+# implement this function so that it returns copy of string_arg reversed
+def reverseWord(string_arg):
+	pass
+
+# TEAM MEMBER:
+# implement this function so that it returns the frequency of query in string_arg
+def countFreq(string_arg, query):
+	pass
+
+def main():
+	data = 'guidorossumwashere'
+	print 'REVERSED ==>', reverseWord(data)
+	print 'FREQUENCY OF s IN', data, '==>', countFreq(data, 's')
+
+if __name__ == "__main__": 
+	main()
