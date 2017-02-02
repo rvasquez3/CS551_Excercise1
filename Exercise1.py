539f7219cd46525c1d193d2cc040169f4eb2c134
hchung1 <hchung1@live.nmhu.edu>
2017-02-02 12:35:49 -0700

    Made the file compatible for python 3

--- Exercise1.py
+++ Exercise1.py
@@ -1,15 +1,14 @@
 # git-exercise-01.py
 
-print '***This is an excercise that teaches you to collaborate through Github!***'
-print 'Team Members: \n 1)Rosie Vasquez\n 2)Henry Chung'
-print 'This is a simple program that allows you to reverse a word(s) and counts the frequecy of a letter in the word(s).'
+print ("***This is an excercise that teaches you to collaborate through Github!***")
+print ("Team Members: \n 1)Rosie Vasquez\n 2)Henry Chung")
+print ("This is a simple program that allows you to reverse a word(s) and counts the frequecy of a letter in the word(s).")
 
 # TEAM LEADER:
 # implement this function so that it returns copy of string_arg reversed
 def reverseWord(string_arg):
 	string_arg = string_arg[::-1]
 	return string_arg	
-	pass
 
 # TEAM MEMBER:
 # implement this function so that it returns the frequency of query in string_arg
@@ -19,13 +18,12 @@ def countFreq(string_arg, query):
 		if string_arg[i] == query:
 			counter += 1
 	return counter
-	pass
 
 def main():	
 	data = 'guidorossumwashere'
-	print 'YOUR WORD IS:', data	
-	print 'REVERSED ==>', reverseWord(data)
-	print 'THE FREQUENCY OF s IN', data, '==>', countFreq(data, 's')
+	print ("YOUR WORD IS:", data)	
+	print ("REVERSED ==>", reverseWord(data))
+	print ("THE FREQUENCY OF s IN", data, "==>", countFreq(data, 's'))
 
 if __name__ == "__main__": 
 	main()
